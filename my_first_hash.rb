c06ab965d455425e1c440c981784a7143779b03d
Megan Fago <meganlaynefago@gmail.com>
2019-01-19 18:39:08 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,5 @@
 def my_hash(family)
   family = { "Husband" => "Chris", "Oldest" => "Coco", "Youngest" => "Phoebe"}
- # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 end
 
 
